5d963ea39ef20fe221ab27e12df4b42dd5af0516
Andrés Felipe Muñoz Ortiz <163070299+Andrewxf3@users.noreply.github.com>
2024-11-12 09:09:15 -0500

    5

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -5,12 +5,14 @@ class Auto {
     int precio;
     Asiento[] asientos;
     String marca;
+    Motor motor;
     int registro;
     static int cantidadCreados;
 
-    public Auto(String modelo, int precio, String marca, Motor motor, int registro) {
+    public Auto(String modelo, int precio, Asiento[] asientos, String marca, Motor motor, int registro) {
         this.modelo = modelo;
         this.precio = precio;
+        this.asientos = asientos;
         this.marca = marca;
         this.motor = motor;
         this.registro = registro;
@@ -18,27 +20,23 @@ class Auto {
     }
 
     public int cantidadAsientos() {
-        int contador = 0;
-        if (asiento1 != null) contador++;
-        if (asiento2 != null) contador++;
-        if (asiento3 != null) contador++;
-        if (asiento4 != null) contador++;
-        if (asiento5 != null) contador++;
-        if (asiento6 != null) contador++;
-        return contador;
+        int count = 0;
+        for (Asiento asiento : asientos) {
+            if (asiento != null) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public String verificarIntegridad() {
-        if (this.registro != motor.registro) {
+        if (motor != null && motor.registro != registro) {
             return "Las piezas no son originales";
         }
-        if ((asiento1 != null && asiento1.registro != this.registro) ||
-            (asiento2 != null && asiento2.registro != this.registro) ||
-            (asiento3 != null && asiento3.registro != this.registro) ||
-            (asiento4 != null && asiento4.registro != this.registro) ||
-            (asiento5 != null && asiento5.registro != this.registro) ||
-            (asiento6 != null && asiento6.registro != this.registro)) {
-            return "Las piezas no son originales";
+        for (Asiento asiento : asientos) {
+            if (asiento != null && asiento.registro != registro) {
+                return "Las piezas no son originales";
+            }
         }
         return "Auto original";
     }
